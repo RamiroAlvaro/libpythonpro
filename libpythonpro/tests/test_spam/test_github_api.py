1f14a443e393c721d9bf36ef7a456b53d51451a8
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-04-23 08:30:39 -0300

    Utilizando Mock en test github_api

---
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -0,0 +1,30 @@
+from unittest.mock import Mock
+
+import pytest
+
+from libpythonpro import github_api
+
+
+@pytest.fixture
+def avatar_url():
+    resp_mock = Mock()
+    url = 'https://avatars2.githubusercontent.com/u/7095750?v=4'
+    resp_mock.json.return_value = {
+        'login': 'RamiroAlvaro',
+        'id': 7095750,
+        'avatar_url': url
+    }
+    get_original = github_api.requests.get
+    github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('RamiroAlvaro')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('RamiroAlvaro')
+    assert 'https://avatars2.githubusercontent.com/u/7095750?v=4' == url
